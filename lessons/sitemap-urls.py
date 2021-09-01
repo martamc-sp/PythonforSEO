571f757dd2a22479f2946969a24051f82ef7aa4f
upzerk <86806059+upzerk@users.noreply.github.com>
2021-09-01 15:51:36 +0300

    first lesson added

---
+++ lessons/sitemap-urls.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+""" Youtube Lesson https://youtu.be/ZaUw8ZYggtw """
+url = 'https://url.com/sitemap.xml'
+sitemapsoup = BeautifulSoup(requests.get(url).content, 'lxml')
+sitemapurls = sitemapsoup.find_all("loc")
+xml_urls = [sitemapurl.text for sitemapurl in sitemapurls]
+count = 0
+for websiteurls in xml_urls:
+    count += 1
+    print(websiteurls)
+    print(count)
