d5c62b36fabf860844284adbac7af369a2c70de1
upzerk <86806059+upzerk@users.noreply.github.com>
2021-09-01 16:16:14 +0300

    urls status code

---
+++ lessons/urls-status-code.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://url.com/sitemap.xml'
+sitemapsoup = BeautifulSoup(requests.get(url).content, 'lxml')
+sitemapurls = sitemapsoup.find_all("loc")
+xml_urls = [sitemapurl.text for sitemapurl in sitemapurls]
+count = 0
+for websiteurls in xml_urls:
+    count += 1
+    response = requests.get(websiteurls)
+    print(websiteurls)
+    print(response)
+    print(count)
