758226aaa72888e5f652245e562ca839f7e325c5
upzerk <86806059+upzerk@users.noreply.github.com>
2021-09-01 16:19:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-""" Everything we do is available in this file. But if you want to view it individually, visit the Lessons file. """
-
 url = 'https://upzerk.com/sitemap.xml'
 sitemapsoup = BeautifulSoup(requests.get(url).content, 'lxml')
 sitemapurls = sitemapsoup.find_all("loc")
@@ -10,5 +8,7 @@ xml_urls = [sitemapurl.text for sitemapurl in sitemapurls]
 count = 0
 for websiteurls in xml_urls:
     count += 1
+    response = requests.get(websiteurls)
     print(websiteurls)
+    print(response)
     print(count)
