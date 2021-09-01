b8a7e1ca6cd1194eb2e95cc63a0a6b6575251824
upzerk <86806059+upzerk@users.noreply.github.com>
2021-09-01 17:54:37 +0300

    canonical finder

---
+++ lessons/urls-canonical.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://url.com/sitemap.xml'
+sitemapsoup = BeautifulSoup(requests.get(url).content, 'lxml')
+sitemapurls = sitemapsoup.find_all("loc")
+xml_urls = [sitemapurl.text for sitemapurl in sitemapurls]
+count = 0
+cerror = 0
+mydata = open("FILEPATH/data.txt", "w")
+for websiteurls in xml_urls:
+    source = BeautifulSoup(requests.get(websiteurls).text , 'html.parser')
+    
+    try:
+        count += 1
+        mydata.write("yes!")
+        mydata.write("\n")
+        mydata.write(source.find('link', {'rel': 'canonical'}) ['href'])
+        mydata.write("\n")
+        print(count)
+    except:
+        mydata.write("no!")
+        mydata.write(websiteurls)
+        cerror += 1
+        print(cerror)
+mydata.close()
