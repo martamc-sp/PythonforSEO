f2996ac55ef16b007d2627a0829129ea8e4a5049
upzerk <86806059+upzerk@users.noreply.github.com>
2021-09-02 21:41:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://url.com/sitemap.xml'
-sitemapsoup = BeautifulSoup(requests.get(url).content, 'lxml')
-sitemapurls = sitemapsoup.find_all("loc")
-xml_urls = [sitemapurl.text for sitemapurl in sitemapurls]
-count = 0
-for websiteurls in xml_urls:
-    source = BeautifulSoup(requests.get(websiteurls).text , 'html.parser')
-    for titles in source.find_all('title'):
-        print(titles.get_text())
+Loading .... :)
